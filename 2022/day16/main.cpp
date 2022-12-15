2cccb903d20f1f23826f6177c03681a9a3d81ac8
Niko Vaisanen <niko.vaisanen@koherent.io>
2022-12-15 11:51:26 +0200

    Prep for 2022 day 16

---
+++ 2022/day16/main.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+namespace {
+std::string runSolution1(std::ifstream& ifs)
+{
+    return "Todo";
+}
+
+std::string runSolution2(std::ifstream& ifs)
+{
+    return "Todo";
+}
+} // namespace
+
+int main(int argc, char** argv)
+{
+    using namespace std::chrono;
+
+    if (argc < 3)
+    {
+        std::cout << "Usage: ./solution <part> <input file>" << std::endl;
+        return 1;
+    }
+
+    const int part = (std::stoi(argv[1]) == 2) ? 2 : 1;
+    const std::string filename(argv[2]);
+    std::ifstream ifs(filename);
+
+    const auto start = high_resolution_clock::now();
+    const std::string output = (part == 1) ? runSolution1(ifs) : runSolution2(ifs);
+    const auto end = high_resolution_clock::now();
+
+    std::cout << "Solution (part " << part << "): " << output << std::endl;
+    std::cout << "Elapsed time: " << duration_cast<milliseconds>(end - start).count() << "ms"
+              << std::endl;
+    return 0;
+}
