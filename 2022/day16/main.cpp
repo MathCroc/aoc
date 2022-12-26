b2df63d936020bc055a661bf0cf2299412823a19
Niko Vaisanen <niko.vaisanen@koherent.io>
2022-12-26 15:55:14 +0200

    Pptimization to 2022 day 16 part 1

--- 2022/day16/main.cpp
+++ 2022/day16/main.cpp
@@ -17,14 +17,29 @@ namespace {
 struct Valve
 {
     int flow;
-    std::vector<std::string> tunnels;
+    std::vector<int> tunnels;
 };
 
-using Valves = std::unordered_map<std::string, Valve>;
+using Valves = std::unordered_map<int, Valve>;
+
+int getIndex(std::unordered_map<std::string, int>& mapping,
+             const std::string& name,
+             int& valveIndex)
+{
+    if (mapping.find(name) == mapping.end())
+    {
+        mapping.insert({ name, valveIndex++ });
+    }
+    return mapping.at(name);
+}
 
 Valves parse(std::ifstream& ifs)
 {
     Valves valves;
+
+    std::unordered_map<std::string, int> mapping;
+    mapping.insert({ "AA", 0 });
+    int valveIndex = 1;
     while (ifs.good())
     {
         std::string line;
@@ -33,6 +48,163 @@ Valves parse(std::ifstream& ifs)
             break;
 
         Valve v;
+        std::string name;
+
+        std::stringstream ss(line);
+        ss >> name;
+        int id = getIndex(mapping, name, valveIndex);
+
+        ss >> v.flow;
+        while (ss.good())
+        {
+            std::string s;
+            ss >> s;
+            if (s.empty())
+                break;
+
+            v.tunnels.push_back(getIndex(mapping, s, valveIndex));
+        }
+        valves.insert({ id, v });
+    }
+    return valves;
+}
+
+int bfs(const Valves& valves, int start, int end)
+{
+    std::unordered_set<int> visited;
+    visited.insert(start);
+    std::vector<int> queue;
+    queue.push_back(start);
+    int steps = 0;
+    while (not queue.empty())
+    {
+        if (visited.find(end) != visited.end())
+        {
+            break;
+        }
+
+        std::vector<int> next;
+        for (int v : queue)
+        {
+            for (int n : valves.at(v).tunnels)
+            {
+                if (visited.find(n) != visited.end())
+                    continue;
+
+                next.push_back(n);
+                visited.insert(n);
+            }
+        }
+        queue = std::move(next);
+        ++steps;
+    }
+
+    if (queue.empty())
+    {
+        throw 1;
+    }
+    return steps;
+}
+
+struct QueueElem
+{
+    int id;
+    int stepsRemaining;
+    int totalReleased;
+    uint32_t open;
+};
+
+std::string runSolution1(std::ifstream& ifs)
+{
+    const auto valves = parse(ifs);
+
+    std::unordered_map<int, int> bitMap;
+    bitMap.insert({ 0, 0 });
+    int bitIndex = 1;
+    for (const auto& [id, v] : valves)
+    {
+        if (v.flow > 0)
+        {
+            bitMap[id] = bitIndex++;
+        }
+    }
+
+    std::vector<std::vector<int>> times(bitMap.size(), std::vector<int>(bitMap.size()));
+    std::vector<int> flows(bitMap.size(), 0);
+    for (auto [id0, index0] : bitMap)
+    {
+        flows[index0] = valves.at(id0).flow;
+        for (auto [id1, index1] : bitMap)
+        {
+            times[index0][index1] = bfs(valves, id0, id1) + 1;
+        }
+    }
+
+    std::unordered_map<uint64_t, int> visited;
+    std::vector<QueueElem> queue{ { 0, 0, 0, 0b1 } };
+    for (int step = 0; step < 30; ++step)
+    {
+        std::vector<QueueElem> next;
+        for (auto q : queue)
+        {
+            --q.stepsRemaining;
+            if (q.stepsRemaining > 0)
+            {
+                next.push_back(q);
+            }
+            else
+            {
+                uint64_t h = ((uint64_t)q.open << 32) | q.id;
+                auto it = visited.find(h);
+                if (it != visited.end() and it->second >= q.totalReleased)
+                    continue;
+
+                visited.insert({ h, q.totalReleased });
+
+                const int totalReleased = q.totalReleased + flows[q.id] * (30 - step);
+                const uint32_t open = q.open | (1u << q.id);
+                for (int i = 1; i < (int)times.size(); ++i)
+                {
+                    if (open & (1u << i))
+                        continue;
+
+                    next.push_back({ i, times[q.id][i], totalReleased, open });
+                }
+            }
+        }
+        queue = std::move(next);
+    }
+
+    int maxReleased = 0;
+    for (const auto& q : queue)
+    {
+        maxReleased = std::max(maxReleased, q.totalReleased);
+    }
+
+    return std::to_string(maxReleased);
+}
+
+/* ----- Old implementation ----- */
+
+struct Valve2
+{
+    int flow;
+    std::vector<std::string> tunnels;
+};
+
+using Valves2 = std::unordered_map<std::string, Valve2>;
+
+Valves2 parse2(std::ifstream& ifs)
+{
+    Valves2 valves;
+    while (ifs.good())
+    {
+        std::string line;
+        std::getline(ifs, line);
+        if (line.empty())
+            break;
+
+        Valve2 v;
         std::string name;
 
         std::stringstream ss(line);
@@ -50,81 +222,6 @@ Valves parse(std::ifstream& ifs)
         valves.insert({ name, v });
     }
     return valves;
-}
-
-struct QueueElem
-{
-    std::string name;
-    int totalReleased;
-    uint32_t open;
-};
-
-void insertIfNeeded(std::vector<QueueElem>& next,
-                    std::unordered_map<uint64_t, int>& visited,
-                    const std::unordered_map<std::string, int>& indexMap,
-                    QueueElem q)
-{
-    uint64_t h = ((uint64_t)q.open << 32) | indexMap.at(q.name);
-    auto it = visited.find(h);
-    if (it != visited.end() and q.totalReleased <= it->second)
-        return;
-
-    visited[h] = q.totalReleased;
-    next.push_back(q);
-}
-
-std::string runSolution1(std::ifstream& ifs)
-{
-    const auto valves = parse(ifs);
-
-    std::unordered_map<std::string, int> indexMap;
-    std::unordered_map<std::string, int> bitMap;
-    int bitIndex = 0;
-    int valveIndex = 0;
-    for (const auto& [name, v] : valves)
-    {
-        indexMap[name] = valveIndex++;
-        if (v.flow > 0)
-        {
-            bitMap[name] = bitIndex++;
-        }
-    }
-
-    std::unordered_map<uint64_t, int> visited;
-    std::vector<QueueElem> queue{ { "AA", 0, 0 } };
-    for (int step = 0; step < 30; ++step)
-    {
-        std::vector<QueueElem> next;
-        for (const auto& q : queue)
-        {
-            auto bitIt = bitMap.find(q.name);
-            if (bitIt != bitMap.end() and (q.open & (1u << bitIt->second)) == 0)
-            {
-                auto v = q;
-                v.totalReleased += valves.at(q.name).flow * (29 - step);
-                v.open |= (1u << bitIt->second);
-                insertIfNeeded(next, visited, indexMap, v);
-            }
-
-            for (const auto& tunnel : valves.at(q.name).tunnels)
-            {
-                auto v = q;
-                v.name = tunnel;
-                insertIfNeeded(next, visited, indexMap, v);
-            }
-        }
-
-        std::cout << "Step: " << step << std::endl;
-        queue = std::move(next);
-    }
-
-    int maxReleased = 0;
-    for (const auto& [h, totalReleased] : visited)
-    {
-        maxReleased = std::max(maxReleased, totalReleased);
-    }
-
-    return std::to_string(maxReleased);
 }
 
 struct State
@@ -160,7 +257,7 @@ void insertIfNeeded(std::vector<State>& next, std::unordered_map<uint64_t, int>&
 
 std::string runSolution2(std::ifstream& ifs)
 {
-    auto valves = parse(ifs);
+    auto valves = parse2(ifs);
 
     std::unordered_map<std::string, uint16_t> indexMap;
     std::unordered_map<std::string, int> bitMap;
