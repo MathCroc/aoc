97df9c7bbb22a762279d7b54876a5bc1497b978f
Niko Vaisanen <niko.vaisanen@koherent.io>
2023-12-03 09:56:53 +0200

    Solutions for 2023 day 3

--- 2023/day3/main.cpp
+++ 2023/day3/main.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <array>
+#include <charconv>
 #include <chrono>
 #include <cstddef>
 #include <cstdint>
@@ -24,36 +25,15 @@ std::vector<std::string> parse(std::ifstream& ifs)
         if (line.empty())
             break;
 
-        out.push_back(line);
+        out.push_back("." + line + ".");
     }
+
+    // Insert some extra dots to avoid boundary checking
+    std::string dummy_row(out[0].size(), '.');
+    out.insert(out.begin(), dummy_row);
+    out.push_back(dummy_row);
+
     return out;
-}
-
-uint64_t toCharMask(const std::string_view& a)
-{
-    uint64_t mask = 0;
-    for (char c : a)
-    {
-        if ('a' <= c and c <= 'z')
-        {
-            mask |= 1ull << (c - 'a' + 1);
-        }
-        else
-        {
-            mask |= 1ull << (c - 'A' + 27);
-        }
-    }
-    return mask;
-}
-
-uint64_t intersect(uint64_t a, uint64_t b)
-{
-    return a & b;
-}
-
-int priority(uint64_t a)
-{
-    return __builtin_ctzll(a);
 }
 
 std::string runSolution1(std::ifstream& ifs)
@@ -61,14 +41,46 @@ std::string runSolution1(std::ifstream& ifs)
     const auto lines = parse(ifs);
 
     int sum = 0;
-    for (const auto& line : lines)
+    for (size_t i = 1; i < lines.size() - 1; ++i)
     {
-        auto view = std::string_view(line);
-        auto first = view.substr(0, view.size() / 2);
-        auto second = view.substr(view.size() / 2);
+        const auto& line = lines[i];
+        size_t pos = 1;
+        while (pos < line.size() - 1)
+        {
+            pos = line.find_first_of("0123456789", pos);
+            if (pos == std::string::npos)
+            {
+                break;
+            }
 
-        auto mask = intersect(toCharMask(first), toCharMask(second));
-        sum += priority(mask);
+            size_t end = pos + 1;
+            while (std::isdigit(line[end]) != 0)
+            {
+                end++;
+            }
+
+            bool is_adjacent = false;
+            for (size_t j = i - 1; j <= i + 1 and not is_adjacent; ++j)
+            {
+                for (size_t k = pos - 1; k <= end; ++k)
+                {
+                    char c = lines[j][k];
+                    if (not std::isdigit(c) and c != '.')
+                    {
+                        is_adjacent = true;
+                        break;
+                    }
+                }
+            }
+
+            if (is_adjacent)
+            {
+                int val{};
+                std::from_chars(line.data() + pos, line.data() + end, val);
+                sum += val;
+            }
+            pos = end;
+        }
     }
 
     return std::to_string(sum);
@@ -78,77 +90,56 @@ std::string runSolution2(std::ifstream& ifs)
 {
     const auto lines = parse(ifs);
 
-    int sum = 0;
-    for (unsigned i = 0; i < lines.size(); i += 3)
-    {
-        auto a = toCharMask(std::string_view(lines[i]));
-        auto b = toCharMask(std::string_view(lines[i + 1]));
-        auto c = toCharMask(std::string_view(lines[i + 2]));
+    std::unordered_map<int, std::vector<int>> gears;
 
-        auto mask = intersect(intersect(a, b), c);
-        sum += priority(mask);
+    for (size_t i = 1; i < lines.size() - 1; ++i)
+    {
+        const auto& line = lines[i];
+        size_t pos = 1;
+        while (pos < line.size() - 1)
+        {
+            pos = line.find_first_of("0123456789", pos);
+            if (pos == std::string::npos)
+            {
+                break;
+            }
+
+            size_t end = pos + 1;
+            while (std::isdigit(line[end]) != 0)
+            {
+                end++;
+            }
+
+            int val{};
+            std::from_chars(line.data() + pos, line.data() + end, val);
+
+            for (size_t j = i - 1; j <= i + 1; ++j)
+            {
+                for (size_t k = pos - 1; k <= end; ++k)
+                {
+                    char c = lines[j][k];
+                    if (c == '*')
+                    {
+                        gears[j * line.size() + k].push_back(val);
+                        break;
+                    }
+                }
+            }
+
+            pos = end;
+        }
     }
 
-    return std::to_string(sum);
-}
-
-[[maybe_unused]] std::string origRunSolution1(std::ifstream& ifs)
-{
-    const auto lines = parse(ifs);
-
     int sum = 0;
-    for (const auto& line : lines)
+    for (const auto& [gear, vals] : gears)
     {
-        auto first = line.substr(0, line.size() / 2);
-        auto second = line.substr(line.size() / 2);
-        std::sort(first.begin(), first.end());
-        std::sort(second.begin(), second.end());
-
-        std::vector<char> common;
-        std::set_intersection(
-            first.begin(), first.end(), second.begin(), second.end(), std::back_inserter(common));
-        char c = common[0];
-        if ('a' <= c and c <= 'z')
+        (void)gear;
+        if (vals.size() != 2)
         {
-            sum += c - 'a' + 1;
+            continue;
         }
-        else
-        {
-            sum += c - 'A' + 27;
-        }
-    }
 
-    return std::to_string(sum);
-}
-
-[[maybe_unused]] std::string origRunSolution2(std::ifstream& ifs)
-{
-    const auto lines = parse(ifs);
-
-    int sum = 0;
-    for (unsigned i = 0; i < lines.size(); i += 3)
-    {
-        auto a = lines[i];
-        auto b = lines[i + 1];
-        auto c = lines[i + 2];
-        std::sort(a.begin(), a.end());
-        std::sort(b.begin(), b.end());
-        std::sort(c.begin(), c.end());
-
-        std::vector<char> ab;
-        std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(ab));
-
-        std::vector<char> common;
-        std::set_intersection(ab.begin(), ab.end(), c.begin(), c.end(), std::back_inserter(common));
-        char tmp = common[0];
-        if ('a' <= tmp and tmp <= 'z')
-        {
-            sum += tmp - 'a' + 1;
-        }
-        else
-        {
-            sum += tmp - 'A' + 27;
-        }
+        sum += vals[0] * vals[1];
     }
 
     return std::to_string(sum);
