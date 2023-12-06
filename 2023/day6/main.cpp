e92e25760425a6340521e27a76e8b05f31a58858
Niko Vaisanen <niko.vaisanen@koherent.io>
2023-12-06 11:17:40 +0200

    Solutions for 2023 day 6

--- 2023/day6/main.cpp
+++ 2023/day6/main.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <array>
 #include <chrono>
+#include <cmath>
 #include <cstddef>
 #include <cstdint>
 #include <fstream>
@@ -14,41 +15,64 @@
 #include <vector>
 
 namespace {
-std::string parse(std::ifstream& ifs)
+std::vector<int> parse(std::ifstream& ifs)
 {
     std::string line;
     std::getline(ifs, line);
-    return line;
+    std::stringstream ss(line);
+
+    std::vector<int> out;
+    while (ss.good())
+    {
+        int val = 0;
+        ss >> val;
+        if (val <= 0)
+            break;
+
+        out.push_back(val);
+    }
+
+    return out;
 }
 
-int findHeader(const std::string& signal, int windowSize)
+long long parse2(std::ifstream& ifs)
 {
-    int index = windowSize;
-    for (; index <= (int)signal.size(); ++index)
-    {
-        uint32_t window = 0;
-        for (int i = index - windowSize; i < index; ++i)
-        {
-            window |= 1u << (signal[i] - 'a');
-        }
-        if (__builtin_popcount(window) == windowSize)
-        {
-            break;
-        }
-    }
-    return index;
+    std::string line;
+    std::getline(ifs, line);
+    auto end = std::remove(line.begin(), line.end(), ' ');
+    line.erase(end, line.end());
+    return std::stoll(line);
+}
+
+long long count(double t, double d)
+{
+    d += 0.1; // Avoid equality
+    double c = std::sqrt(t * t - 4 * d);
+    int a = std::ceil((t - c) / 2);
+    int b = std::floor((t + c) / 2);
+    int n = b - a + 1;
+    return n;
 }
 
 std::string runSolution1(std::ifstream& ifs)
 {
-    const auto signal = parse(ifs);
-    return std::to_string(findHeader(signal, 4));
+    auto times = parse(ifs);
+    auto distances = parse(ifs);
+
+    long long tot = 1;
+    for (int i = 0; i < (int)times.size(); ++i)
+    {
+        tot *= count(times[i], distances[i]);
+    }
+
+    return std::to_string(tot);
 }
 
 std::string runSolution2(std::ifstream& ifs)
 {
-    const auto signal = parse(ifs);
-    return std::to_string(findHeader(signal, 14));
+    double t = parse2(ifs);
+    double d = parse2(ifs);
+    return std::to_string(count(t, d));
 }
 } // namespace
 
