988cd131ec6990accaa56a99d931c78ea2315036
Niko Vaisanen <niko.vaisanen@koherent.io>
2023-12-18 22:04:49 +0200

    Solutions to 2023 day 18

--- 2023/day18/main.cpp
+++ 2023/day18/main.cpp
@@ -16,247 +16,458 @@
 #include <vector>
 
 namespace {
-struct Cube
+struct Dig
 {
-    int16_t x;
-    int16_t y;
-    int16_t z;
+    char dir;
+    int count;
+    std::string color;
 };
 
-Cube add(Cube a, Cube b)
+std::vector<Dig> parse(std::ifstream& ifs)
 {
-    return { (int16_t)(a.x + b.x), (int16_t)(a.y + b.y), (int16_t)(a.z + b.z) };
-}
-
-using Cubes = std::array<std::array<std::array<bool, 25>, 25>, 25>;
-
-Cubes parse(std::ifstream& ifs)
-{
-    Cubes cubes{};
-    int x, y, z;
+    std::vector<Dig> plan;
     while (ifs.good())
     {
-        ifs >> x;
-        ifs.ignore(1);
-        ifs >> y;
-        ifs.ignore(1);
-        ifs >> z;
-        ifs.ignore(1);
-        cubes[z + 2][y + 2][x + 2] = true;
+        std::string line;
+        std::getline(ifs, line);
+        if (line.empty())
+            break;
+
+        Dig d;
+        d.dir = line[0];
+        d.count = std::stoi(line.substr(2));
+
+        auto pos = line.find("#");
+        d.color = line.substr(pos + 1, 6);
+        plan.push_back(d);
     }
-    return cubes;
+    return plan;
+}
+
+int bfs(std::vector<std::string>& grid, int row, int col)
+{
+    constexpr std::array<std::pair<int, int>, 4> diffs{
+        { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } }
+    };
+
+    bool inside = true;
+    std::vector<std::pair<int, int>> queue;
+    queue.push_back({ row, col });
+    grid[row][col] = 'x';
+    int count = 1;
+    while (not queue.empty())
+    {
+        std::vector<std::pair<int, int>> next;
+        for (auto [r, c] : queue)
+        {
+            for (auto [dr, dc] : diffs)
+            {
+                int rr = r + dr;
+                int cc = c + dc;
+                if (rr < 0 or cc < 0 or rr >= (int)grid.size() or cc >= (int)grid[0].size())
+                {
+                    inside = false;
+                    continue;
+                }
+
+                if (grid[rr][cc] != '.')
+                    continue;
+
+                grid[rr][cc] = 'x';
+                next.push_back({ rr, cc });
+                count++;
+            }
+        }
+        queue = std::move(next);
+    }
+
+    return inside ? count : 0;
+}
+
+long long bfs2(std::vector<std::vector<bool>>& grid,
+               const std::vector<int>& xs,
+               const std::vector<int>& ys,
+               int row,
+               int col)
+{
+    constexpr std::array<std::pair<int, int>, 4> diffs{
+        { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } }
+    };
+
+    bool inside = true;
+    std::vector<std::pair<int, int>> queue;
+    queue.push_back({ row, col });
+    grid[row][col] = true;
+    long long count = (long long)(xs[col + 1] - xs[col]) * (long long)(ys[row + 1] - ys[row]);
+    while (not queue.empty())
+    {
+        std::vector<std::pair<int, int>> next;
+        for (auto [r, c] : queue)
+        {
+            for (auto [dr, dc] : diffs)
+            {
+                int rr = r + dr;
+                int cc = c + dc;
+                if (rr < 0 or cc < 0 or rr >= (int)grid.size() or cc >= (int)grid[0].size())
+                {
+                    inside = false;
+                    continue;
+                }
+
+                if (grid[rr][cc])
+                    continue;
+
+                grid[rr][cc] = true;
+                next.push_back({ rr, cc });
+                count += (long long)(xs[cc + 1] - xs[cc]) * (long long)(ys[rr + 1] - ys[rr]);
+            }
+        }
+        queue = std::move(next);
+    }
+
+    return inside ? count : 0;
+}
+
+long long count(const std::vector<Dig>& plan)
+{
+    // For the non-uniform grid
+    std::vector<int> xs;
+    std::vector<int> ys;
+
+    // Lower right corner coordinates
+    int x = 0;
+    int y = 0;
+
+    for (const auto& d : plan)
+    {
+        switch (d.dir)
+        {
+            case '0': // right
+            case 'R':
+                xs.push_back(x - 1);
+                xs.push_back(x + d.count);
+                ys.push_back(y - 1);
+                ys.push_back(y);
+                x += d.count;
+                break;
+            case '1': // down
+            case 'D':
+                xs.push_back(x - 1);
+                xs.push_back(x);
+                ys.push_back(y - 1);
+                ys.push_back(y + d.count);
+                y += d.count;
+                break;
+            case '2': // left
+            case 'L':
+                xs.push_back(x);
+                xs.push_back(x - d.count - 1);
+                ys.push_back(y - 1);
+                ys.push_back(y);
+                x -= d.count;
+                break;
+            case '3': // up
+            case 'U':
+                xs.push_back(x - 1);
+                xs.push_back(x);
+                ys.push_back(y);
+                ys.push_back(y - d.count - 1);
+                y -= d.count;
+                break;
+            default:
+                throw 1;
+        }
+    }
+
+    std::sort(xs.begin(), xs.end());
+    std::sort(ys.begin(), ys.end());
+
+    auto xIt = std::unique(xs.begin(), xs.end());
+    xs.erase(xIt, xs.end());
+
+    auto yIt = std::unique(ys.begin(), ys.end());
+    ys.erase(yIt, ys.end());
+
+    // std::cout << "xs: ";
+    // for (auto v : xs)
+    // {
+    //     std::cout << v << " ";
+    // }
+    // std::cout << std::endl << "ys: ";
+    // for (auto v : ys)
+    // {
+    //     std::cout << v << " ";
+    // }
+    // std::cout << std::endl;
+
+    // Mark the loop
+    std::vector<std::vector<bool>> grid(ys.size(), std::vector<bool>(xs.size()));
+    long long count = 0;
+
+    (void)grid;
+
+    x = 0;
+    y = 0;
+
+    xIt = std::lower_bound(xs.begin(), xs.end(), x);
+    yIt = std::lower_bound(ys.begin(), ys.end(), y);
+
+    int xInd = std::distance(xs.begin(), xIt) - 1;
+    int yInd = std::distance(ys.begin(), yIt) - 1;
+    grid[yInd][xInd] = true;
+
+    for (const auto& d : plan)
+    {
+        switch (d.dir)
+        {
+            case '0': // right
+            case 'R':
+                for (x += d.count; xs[xInd + 1] < x; ++xInd)
+                {
+                    // std::cout << x << " " << y << " " << xInd << " " << yInd << std::endl;
+                    grid[yInd][xInd] = true;
+                    count += xs[xInd + 1] - xs[xInd];
+                }
+                break;
+            case '1': // down
+            case 'D':
+                for (y += d.count; ys[yInd + 1] < y; ++yInd)
+                {
+                    // std::cout << x << " " << y << " " << xInd << " " << yInd << std::endl;
+                    grid[yInd][xInd] = true;
+                    count += ys[yInd + 1] - ys[yInd];
+                }
+                break;
+            case '2': // left
+            case 'L':
+                for (x -= d.count; xs[xInd + 1] > x; --xInd)
+                {
+                    // std::cout << x << " " << y << " " << xInd << " " << yInd << std::endl;
+                    grid[yInd][xInd] = true;
+                    count += xs[xInd + 1] - xs[xInd];
+                }
+                break;
+            case '3': // up
+            case 'U':
+                for (y -= d.count; ys[yInd + 1] > y; --yInd)
+                {
+                    // std::cout << x << " " << y << " " << xInd << " " << yInd << std::endl;
+                    grid[yInd][xInd] = true;
+                    count += ys[yInd + 1] - ys[yInd];
+                }
+                break;
+            default:
+                throw 1;
+        }
+    }
+
+    // for (const auto& row : grid)
+    // {
+    //     for (auto b : row)
+    //     {
+    //         if (b)
+    //         {
+    //             std::cout << "#";
+    //         }
+    //         else
+    //         {
+    //             std::cout << ".";
+    //         }
+    //     }
+    //     std::cout << std::endl;
+    // }
+
+    for (int row = 0; row < (int)grid.size(); ++row)
+    {
+        for (int col = 0; col < (int)grid[row].size(); ++col)
+        {
+            if (grid[row][col])
+                continue;
+
+            count += bfs2(grid, xs, ys, row, col);
+        }
+    }
+
+    std::cout << count << std::endl;
+
+    return count;
 }
 
 std::string runSolution1(std::ifstream& ifs)
 {
-    const auto cubes = parse(ifs);
-    int count = 0;
-    for (unsigned z = 1; z < cubes.size() - 1; ++z)
-    {
-        for (unsigned y = 1; y < cubes[z].size() - 1; ++y)
-        {
-            for (unsigned x = 1; x < cubes[z][y].size() - 1; ++x)
-            {
-                if (not cubes[z][y][x])
-                    continue;
+    const auto plan = parse(ifs);
+    count(plan);
 
-                count += !cubes[z - 1][y][x];
-                count += !cubes[z + 1][y][x];
-                count += !cubes[z][y - 1][x];
-                count += !cubes[z][y + 1][x];
-                count += !cubes[z][y][x - 1];
-                count += !cubes[z][y][x + 1];
-            }
+    int xMin = 0;
+    int xMax = 0;
+    int yMin = 0;
+    int yMax = 0;
+    int x = 0;
+    int y = 0;
+    for (const auto& d : plan)
+    {
+        switch (d.dir)
+        {
+            case 'R':
+                x += d.count;
+                break;
+            case 'L':
+                x -= d.count;
+                break;
+            case 'U':
+                y -= d.count;
+                break;
+            case 'D':
+                y += d.count;
+                break;
+            default:
+                throw 1;
+        }
+
+        xMin = std::min(xMin, x);
+        xMax = std::max(xMax, x);
+        yMin = std::min(yMin, y);
+        yMax = std::max(yMax, y);
+    }
+
+    const int width = xMax - xMin + 1;
+    const int height = yMax - yMin + 1;
+    std::vector<std::string> grid(height, std::string(width, '.'));
+
+    x = -xMin;
+    y = -yMin;
+    grid[y][x] = '#';
+    for (const auto& d : plan)
+    {
+        switch (d.dir)
+        {
+            case 'R':
+                for (int i = 0; i < d.count; ++i, ++x)
+                {
+                    grid[y][x] = '#';
+                }
+                break;
+            case 'L':
+                for (int i = 0; i < d.count; ++i, --x)
+                {
+                    grid[y][x] = '#';
+                }
+                break;
+            case 'U':
+                for (int i = 0; i < d.count; ++i, --y)
+                {
+                    grid[y][x] = '#';
+                }
+                break;
+            case 'D':
+                for (int i = 0; i < d.count; ++i, ++y)
+                {
+                    grid[y][x] = '#';
+                }
+                break;
+            default:
+                throw 1;
         }
     }
+
+    int count = 0;
+    for (int row = 0; row < (int)grid.size(); ++row)
+    {
+        for (int col = 0; col < (int)grid[row].size(); ++col)
+        {
+            if (grid[row][col] == 'x')
+                continue;
+
+            if (grid[row][col] == '#')
+                count++;
+            else
+                count += bfs(grid, row, col);
+        }
+    }
+
     return std::to_string(count);
 }
 
 std::string runSolution2(std::ifstream& ifs)
 {
-    const auto cubes = parse(ifs);
-
-    const std::vector<Cube> neighbors{ { -1, 0, 0 }, { 1, 0, 0 },  { 0, -1, 0 },
-                                       { 0, 1, 0 },  { 0, 0, -1 }, { 0, 0, 1 } };
-
-    Cubes visited{};
-
-    // BFS for air cubes
-    std::vector<Cube> queue{ { 1, 1, 1 } };
-    int count = 0;
-    while (not queue.empty())
+    auto plan = parse(ifs);
+    for (auto& d : plan)
     {
-        std::vector<Cube> nextQueue;
-        for (const auto& a : queue)
-        {
-            auto [x, y, z] = a;
-            count += cubes[z - 1][y][x];
-            count += cubes[z + 1][y][x];
-            count += cubes[z][y - 1][x];
-            count += cubes[z][y + 1][x];
-            count += cubes[z][y][x - 1];
-            count += cubes[z][y][x + 1];
+        d.count = std::stoll(d.color.substr(0, 5), nullptr, 16);
+        d.dir = d.color[5];
 
-            for (const auto& n : neighbors)
-            {
-                auto tmp = add(a, n);
-                if (tmp.x < 1 or tmp.x > 23)
-                    continue;
-                if (tmp.y < 1 or tmp.y > 23)
-                    continue;
-                if (tmp.z < 1 or tmp.z > 23)
-                    continue;
-                if (visited[tmp.z][tmp.y][tmp.x])
-                    continue;
-                if (cubes[tmp.z][tmp.y][tmp.x])
-                    continue;
-
-                visited[tmp.z][tmp.y][tmp.x] = true;
-                nextQueue.push_back(tmp);
-            }
-        }
-
-        queue = std::move(nextQueue);
+        std::cout << d.count << " " << d.dir << std::endl;
     }
 
-    return std::to_string(count);
+    return std::to_string(count(plan));
 }
 
-// ----- Original solutions -----
+// struct Segment
+// {
+//     long long level;
+//     long long start;
+//     long long len; // Contains both the starting and ending points
+// };
 
-std::vector<Cube> origParse(std::ifstream& ifs)
-{
-    std::vector<Cube> cubes;
-    while (ifs.good())
-    {
-        Cube c{};
-        ifs >> c.x;
-        ifs.ignore(1);
-        ifs >> c.y;
-        ifs.ignore(1);
-        ifs >> c.z;
-        ifs.ignore(1);
-        cubes.push_back(c);
-    }
-    return cubes;
-}
+// long long countRow(const std::vector<Segment>& vertical,
+//                    const std::vector<Segment>& horizontal,
+//                    long long row)
+// {
+//     auto [first, last] = std::equal_range(
+//         horizontal.begin(), horizontal.end(), row, [](auto a, auto b) { return a < b.level; });
 
-uint64_t hash(Cube c)
-{
-    uint64_t tmp = 0;
-    std::memcpy(&tmp, &c, sizeof(c));
-    return tmp;
-}
+//     if (first != last)
+//     {
+//     }
 
-Cube toCube(uint64_t h)
-{
-    Cube c;
-    std::memcpy(&c, &h, sizeof(Cube));
-    return c;
-}
+//     return 0;
+// }
 
-[[maybe_unused]] std::string origRunSolution1(std::ifstream& ifs)
-{
-    const auto cubes = origParse(ifs);
-    std::unordered_set<uint64_t> points;
-    for (const auto c : cubes)
-    {
-        points.insert(hash(c));
-    }
+// std::string runSolution2(std::ifstream& ifs)
+// {
+//     const auto plan = parse(ifs);
 
-    const std::vector<Cube> neighbors{ { -1, 0, 0 }, { 1, 0, 0 },  { 0, -1, 0 },
-                                       { 0, 1, 0 },  { 0, 0, -1 }, { 0, 0, 1 } };
+//     std::vector<Segment> vertical;
+//     std::vector<Segment> horizontal;
+//     int x = 0;
+//     int y = 0;
+//     for (const auto& d : plan)
+//     {
+//         const long long distance = std::stoll(d.color.substr(0, 5), nullptr, 16);
+//         const char dir = d.color[5];
 
-    int count = 0;
-    for (const auto c : cubes)
-    {
-        for (const auto& n : neighbors)
-        {
-            if (points.find(hash(add(c, n))) == points.end())
-                ++count;
-        }
-    }
-    return std::to_string(count);
-}
+//         switch (dir)
+//         {
+//             case '0': // right
+//                 horizontal.push_back({ .level = y, .start = x, .len = distance + 1 });
+//                 x += distance;
+//                 break;
+//             case '1': // down
+//                 vertical.push_back({ .level = x, .start = y, .len = distance + 1 });
+//                 y += d.count;
+//                 break;
+//             case '2': // left
+//                 horizontal.push_back({ .level = y, .start = x - distance, .len = distance + 1 });
+//                 x -= d.count;
+//                 break;
+//             case '3': // up
+//                 vertical.push_back({ -level = x, .start = y - distance, .len = distance + 1 });
+//                 y -= d.count;
+//                 break;
+//             default:
+//                 throw 1;
+//         }
+//     }
 
-[[maybe_unused]] std::string origRunSolution2(std::ifstream& ifs)
-{
-    const auto cubes = origParse(ifs);
-    std::unordered_set<uint64_t> points;
-    for (const auto c : cubes)
-    {
-        points.insert(hash(c));
-    }
+//     auto seg_comp = [](auto a, auto b) {
+//         return (a.level == b.level) ? a.start < b.start : a.level < b.level;
+//     };
 
-    const std::vector<Cube> neighbors{ { -1, 0, 0 }, { 1, 0, 0 },  { 0, -1, 0 },
-                                       { 0, 1, 0 },  { 0, 0, -1 }, { 0, 0, 1 } };
+//     std::sort(vertical.begin(), vertical.end(), seg_comp);
+//     std::sort(horizontal.begin(), horizontal.end(), seg_comp);
 
-    std::unordered_set<uint64_t> air;
-    int16_t maxX = 0;
-    Cube out{};
-    for (const auto c : cubes)
-    {
-        for (const auto& n : neighbors)
-        {
-            auto h = hash(add(c, n));
-            if (points.find(h) == points.end())
-            {
-                air.insert(h);
-                auto tmp = toCube(h);
-                if (tmp.x > maxX)
-                {
-                    maxX = tmp.x;
-                    out = tmp;
-                }
-            }
-        }
-    }
-
-    // Expand air
-    auto tmpAir = air;
-    for (auto h : tmpAir)
-    {
-        auto c = toCube(h);
-        for (const auto& n : neighbors)
-        {
-            if (points.find(hash(add(c, n))) == points.end())
-                air.insert(hash(add(c, n)));
-        }
-    }
-
-    std::unordered_set<uint64_t> visited;
-    visited.insert(hash(out));
-
-    // BFS for air cubes
-    std::vector<Cube> queue{ out };
-    int count = 0;
-    while (not queue.empty())
-    {
-        std::vector<Cube> nextQueue;
-        for (const auto& a : queue)
-        {
-            for (const auto& n : neighbors)
-            {
-                if (points.find(hash(add(a, n))) != points.end())
-                    ++count;
-            }
-
-            for (const auto& n : neighbors)
-            {
-                auto h = hash(add(a, n));
-                if (visited.find(h) != visited.end() or air.find(h) == air.end())
-                    continue;
-
-                visited.insert(h);
-                nextQueue.push_back(toCube(h));
-            }
-        }
-
-        queue = std::move(nextQueue);
-    }
-
-    return std::to_string(count);
-}
+//     return "";
+// }
 } // namespace
 
 int main(int argc, char** argv)
